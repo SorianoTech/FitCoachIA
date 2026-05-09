3c284ecedff79014c122f0ecfb273c4f7311c3ce
Raul <rulox.github@gmail.com>
2026-05-09 18:41:38 +0200

    Actions build and release, custom action for python installation, TEST endpoint to test workflow, pyproject.toml for configuration, new makefile commands

--- src/fitcoach/main.py
+++ src/fitcoach/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI
 
+from fitcoach.api.router import router
+
 app = FastAPI(title="FitCoach IA - API de Prueba")
+app.include_router(router)
+
 
 @app.get("/")
 async def root():
     return {
         "message": "¡FitCoach IA está funcionando!",
         "status": "online",
-        "version": "0.1.0"
+        "version": "0.1.0",
     }
+
 
 @app.get("/health")
 async def health_check():
